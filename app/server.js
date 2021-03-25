00c5c14f55a7d1b64c779b88ced7205807dc6910
Adrian Moreno <adrian@morenomartinez.com>
2021-03-25 20:01:47 +0100

    Change app background color
    Signed-off-by: Adrian Moreno <adrian@morenomartinez.com>

--- app/server.js
+++ app/server.js
@@ -1,27 +1,27 @@
 var express = require('express');
-var exphbs  = require('express-handlebars');
+var exphbs = require('express-handlebars');
 var app = express();
 var os = require("os");
-var morgan  = require('morgan');
+var morgan = require('morgan');
 
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 app.use(express.static('static'));
 app.use(morgan('combined'));
 
 // Configuration
 var port = process.env.PORT || 8080;
-var message = process.env.MESSAGE || "Hello world!";
+var message = process.env.MESSAGE || "Hello TgnDevs!!!!";
 var contextPath = process.env.CONTEXT_PATH || "/";
 
 app.get('/', function (req, res) {
-    res.render('home', {
-      message: message,
-      platform: os.type(),
-      release: os.release(),
-      hostName: os.hostname(),
-      contextPath: contextPath
-    });
+  res.render('home', {
+    message: message,
+    platform: os.type(),
+    release: os.release(),
+    hostName: os.hostname(),
+    contextPath: contextPath
+  });
 });
 
 // Set up listener
